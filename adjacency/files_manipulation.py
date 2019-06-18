954dc8d1939c12b145e86a9a8310faeab30c1a95
gabriel.igorq <gabriel.igorq@gmail.com>
2019-06-17 23:16:22 -0300

    Mostrando relatório no terminal

--- adjacency/files_manipulation.py
+++ adjacency/files_manipulation.py
@@ -37,6 +37,7 @@ def file_to_list(file):
 		graph[int(node[0])].append(int(node[1]))
 		graph[int(node[1])].append(int(node[0]))
 
+	
 	return graph
 
 '''
@@ -60,16 +61,25 @@ def create_report(g, h, are_isomo, alpha):
 	name_output = input("\nInsira nome do arquivo do relatório (digite .txt ao final do nome): ")
 	file_result = open(name_output, "w")
 
+	print('\n### RELATÓRIO DO EXPERIMENTO ###')
+
+	print('graph G')
 	file_result.write('graph G\n')
+	print(g + '\n\n')
 	file_result.write(g + '\n\n')
 
+	print('graph H')
 	file_result.write('graph H\n')
+	print(h + '\n\n')
 	file_result.write(h + '\n\n')
 
 	if(are_isomo):
+		print('G e H são isomorfos!\n')
 		file_result.write('G e H são isomorfos!\n')
+		print(create_bijection(alpha))
 		file_result.write(create_bijection(alpha))
 	else:
+		print('G e H não são isomorfos!')
 		file_result.write('G e H não são isomorfos!')
 
 	file_result.close()
